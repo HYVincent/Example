3d34dd6584e936124e9cdf2de2ab7d6da171a43b
vincent <1032006226@qq.com>
2016-10-25 12:37:18 +0800

    2016年10月25日12:37:17

---
+++ vincent/src/main/java/com/vincent/libary/listener/ShowNotificationListener.java
@@ -0,0 +1,15 @@
+package com.vincent.libary.listener;
+
+/**
+ * @name Example
+ * @class name：com.vincent.libary.listener
+ * @class describe
+ * @anthor Vincent QQ:1032006226
+ * @time 2016/10/25 11:42
+ * @change
+ * @chang time
+ * @class describe
+ */
+public interface ShowNotificationListener {
+    void showNotification();
+}
